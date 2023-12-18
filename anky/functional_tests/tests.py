8666735e9599e298b03808b4507a2fa41215e001
dims <8914843.pc@gmail.com>
2023-12-18 08:02:14 +0300

    added a second user to the test

--- anky/functional_tests/tests.py
+++ anky/functional_tests/tests.py
@@ -6,8 +6,11 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time 
 
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
     '''тест нового посетителя'''
@@ -22,11 +25,19 @@ class NewVisitorTest(LiveServerTestCase):
         '''демонтаж'''
         self.browser.quit()
 
-    def check_for_row_in_list_talbe(self, row_text):
+    def wait_for_row_in_list_talbe(self, row_text):
         '''подтверждение строки в таблице списка'''
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try: 
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return 
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
@@ -43,7 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите вопрос')
         # Мы вводим в текстовом поле для вопроса "Автор теории относительности"
         inputbox.send_keys('Автор теории относительности')
-        time.sleep(2)
+        
 
         # inputbox = self.browser.find_element(By.ID, 'id_new_item_answer')
         # self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите ответ')
@@ -55,15 +66,12 @@ class NewVisitorTest(LiveServerTestCase):
         # Когда мы нажимаем Enter, страница обновляется и теперь 
         # страница содержит "Вопрос: Автор теории относительности. Ответ: Эйнштейн" 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)
+       
 
-        self.check_for_row_in_list_talbe('Вопрос: Автор теории относительности')
+        self.wait_for_row_in_list_talbe('Вопрос: Автор теории относительности')
         # self.check_for_row_in_list_talbe('Ответ: Эйнштейн')               
-        self.fail('Закончить тест')
         
     
-        # 
-
         # Когда мы нажимаем Enter, страница обновляется и теперь страница содержит "Вопрос: Автор теории относительности. Ответ: Эйнштейн". Ниже
         # находится кнопка подтвердить.
         # inputbox.send_keys(Keys.ENTER)
@@ -77,18 +85,20 @@ class NewVisitorTest(LiveServerTestCase):
         # Мы нажимаем кнопку, страница обновляется и появляется надпись "Запись внесена. Хотите добавить запись?" Внизу две кнопки Добавить и Далее.
 
         # Мы нажимаем кнопку Добавить и появляется текстовое поле с предложением ввести новую запись.
-
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите вопрос')
         # Мы вводим в текстовом поле "Дата начала 2-й мировой войны"
+        inputbox.send_keys('Дата начала 2-й мировой войны')
 
         # Когда мы нажимаем Enter, страница обновляется и теперь страница содержит "Вопрос: Дата начала 2-й мировой войны. Введите ответ" с полем для ответа
-
+        inputbox.send_keys(Keys.ENTER)
         # Мы вводим в поле для ответа ответ "1939 год"
 
         # Когда мы нажимаем Enter, страница обновляется и теперь страница содержит "Вопрос: Дата начала 2-й мировой войны. Ответ: 1939 год". Ниже
         # находится кнопка подтвердить.
-
+        self.wait_for_row_in_list_talbe('Вопрос: Дата начала 2-й мировой войны')
         # Мы нажимаем кнопку, страница обновляется и появляется надпись "Запись внесена. Хотите добавить запись?" Внизу две кнопки Добавить и Далее.
-
+        self.fail('Закончить тест')
         # Мы нажимаем кнопку Далее.
 
         # Мы проверяем, сохранились ли наши записи
@@ -96,6 +106,50 @@ class NewVisitorTest(LiveServerTestCase):
         # Мы видим, что сайт сгенерировал уникальный URL-адрес - об этом выводится текст с пояснением.
 
         # Мы посещаем эти URL-адреса - наши записи там.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        '''тест: многочисленные пользователи могут начать списки по разным url'''
+        # 1-й пользователь начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Автор теории относительности')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_talbe('Вопрос: Автор теории относительности')
+
+        # он замечает, что его список имеет уникальный URL-АДРЕС
+        first_user_list_url = self.browser.current_url
+        self.assertRegex(first_user_list_url, '/lists/.+')
+
+        # 2-й пользователь пришел на сайт
+        # Мы используем новый сеанс браузера, чтобы никакая информация от 1-го
+        # пользователя не прошла через данные cookie 
+        self.browser.quit()
+        options = ChromeOptions()
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.browser = webdriver.Chrome(service=service, options=options)
+
+        # 2-й пользователь посещает домашнюю страницу. На ней нет признаков списка 1-го пользователя
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.asssertNotIn('Автор теории относительности', page_text)
+        self.assertNotIn('Дата начала 2-й мировой войны', page_text)
+
+        # 2-й пользователь начинает новый список, вводя новый элемент
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Самый сложный язык')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_talbe('Вопрос: Самый сложный язык')
+
+        # 2-й пользователь получает уникальный URL-адрес
+        second_user_list_url = self.browser.current_url
+        self.assertRegex(second_user_list_url, '/lists/.+')
+        self.assertNotEqual(second_user_list_url, first_user_list_url)
+
+        # Нет ни следа от списка 1-го пользователя
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text 
+        self.assertNotIn('Дата начала 2-й мировой войны', page_text)
+        self.assertIn('Самый сложный язык', page_text) 
+        
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')     
